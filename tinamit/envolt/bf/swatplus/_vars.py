9cb5ca42d06cd12378274159705876a975595826
James Julius <james.julius@mail.mcgill.ca>
2019-10-04 18:20:16 -0400

    Plenty of empty spaces and mistakes which need to be corrected.

--- tinamit/envolt/bf/swatplus/_vars.py
+++
@@ -1,5 +0,0 @@
-#Una lista de variables SWATPLUS.Ver la documentación SWATPLUS para más detalles.
-from tinamit.mod import VariablesMod
-
-
-class VariablesSWATPLUS(VariablesMod):
--- tinamit/envolt/bf/swatplus/envolt.py
+++ tinamit/envolt/bf/swatplus/envolt.py
@@ -1,68 +1,60 @@
-import os
-
 from tinamit.envolt.bf import ModeloBF
 
-
 class ModeloSWATPLUS(ModeloBF):
-    """
-    Envoltura para modelos SWAT+.
-    """
+    idioma_orig = 'en'
+    def __init__(símismo, variables, nombre = 'SWATPLUS'):
+        símismo.variables = variables
+        símismo.nombre = nombre
+        símismo.corrida = 1
+        símismo.vars_clima = {}
 
-    idioma_orig = 'en'  # La lengua de los nombres y descripción de los variables (y NO la del código aquí)
+        super.__init__(variables,nombre)
 
-    def __init__(símismo, archivo, nombre='SWAT+'):
-
-        símismo.archivo = archivo
-
-        símismo.direc_trabajo = ''
-
-        # Buscar la ubicación del modelo SWAT+.
-        símismo.exe_SWATPLUS = símismo.obt_conf(
-            'exe',
-            cond=os.path.isfile,
-            mnsj_err=_(
-                'Debes especificar la ubicación del ejecutable SWATPLUS, p. ej.'
-                '\n\tModeloSWATPLUS.estab_conf("exe", "C:\\Camino\\hacia\\mi\\SWATPLUSConsole.exe")'
-                '\npara poder hacer simulaciones con modelos SWATPLUS.'
-            )
-        )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        variables = VariablesSWATPLUS(inic=símismo.dic_ingr)
-
-        # Inicializar la clase pariente.
-        super().__init__(variables, nombre=nombre)
-
-    def incrementar(símismo, rebanada):
-        super().incrementar(rebanada)
 
     def unidad_tiempo(símismo):
-        return 'dias' #days
-        return 'horas' #hours
-        return 'Intervalos de 15 minutos' #15 minute intervals
-        return 'minutos' #minutes
-        return 'incrementos' #increments
+        if STEP = 0: #we have to first import the variables file.
+            return "Dias"
+        else if STEP = 1:
+            return "Incrementos"
+        else if STEP = 24:
+            return "Horas"
+        else if STEP = 96:
+            return "Incrementos de 15 minutos"
+        else if STEP = 1440:
+            return 'minutos'
+
+
+    def incrementar(símismo, rebanada):
+
+
+        super().incrementar(rebanada)
+
+    def paralelizable(símismo):
+        return True
+
+    def iniciar_modelo(símismo, corrida):
+        símismo.corrida=corrida
+
+        super().iniciar_modelo(corrida)
+
+    def cerrar(símismo):
+
+    def _correr_hasta_final(símismo):
+        return None
+    #TODO
+
+
+    @classmethod
+    def instalado(cls):
+        return True
+
+    @classmethod
+    def prb_egreso(cls):
+
+    @classmethod
+    def prb_ingreso(cls):
+
+    @classmethod
+    def prb_simul(cls):
+
+#class method should be defined
